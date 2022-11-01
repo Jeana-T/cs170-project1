2f2dbcdcbaa43ec67d353fb6ecacaecd4ee57681
Jeana-T <jeana2019@gmail.com>
2022-11-01 13:38:23 -0700

    created a duplicate check

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import copy
 import bisect
 
 class Node: #Creating a Node class that contains the current puzzle board state, the parent puzzle state, h score, and g score.
-    def __init__(self, currentPuzzleState, parent, h, g):
+    def __init__(self, currentPuzzleState, h, g, previousMove):
         self.currentPuzzleState = currentPuzzleState
-        self.parent = parent #parent element will be used to avoid duplicating it when creating children states
         self.h = h #h score will be found depending on type of heursitc, heurstic function will be called to do calculation
         self.g = g # g = parent depth + 1
+        self.previousMove = previousMove
     
 def empty(nodes): #returns true if nodes queue is empty
     if not nodes: 
@@ -30,7 +30,13 @@ def findCoordinates(puzzleState, value): #finds the location of specified value 
             if puzzleState[x][y] == value:
                 return (x,y) #returns coordinates of specified value
 
-def expand(node, problemOperators):
+def isDuplicateCheck(puzzleState, listNodes):
+    for g in range(len(listNodes)):
+        if puzzleState == listNodes[g].currentPuzzleState:
+            return True
+    return False
+
+def expand(node, problemOperators, expanded):
     blankTile = findCoordinates(node.currentPuzzleState, 0) #coordinates of 0 (blank tile) 
     x, y = blankTile[0], blankTile[1] # x -> column # of blank tile, y -> row # of blank tile
 
@@ -44,35 +50,42 @@ def expand(node, problemOperators):
 
         if a < 0 or a > 2 or b < 0 or b > 2: #if move is illegal, move on
             continue
+        elif (problemOperators[i] == node.previousMove): #stop repeating move to parent puzzle state
+            continue
         else:
             #swap selected tile (at a, b) and blank tile
             nodeStateCopy[x][y], nodeStateCopy[a][b] = nodeStateCopy[a][b], nodeStateCopy[x][y]
 
-            #add temp node (with switched tiles) to children array
-            children.append(Node(nodeStateCopy, node.currentPuzzleState, node.g + 1, 0))
+            if isDuplicateCheck(nodeStateCopy, expanded):
+                continue
+            else:
+                #add temp node (with switched tiles) to children array
+                children.append(Node(nodeStateCopy, 0, (node.g + 1), problemOperators[i]))
     return children
 
 def uniformCostSearchQueue(nodes, children):    
-    for i in range(len(children)):
-        #bisect.insort(nodes, children[i])
-        children[i].h = 0 #uniform cost search is A* w/ h being hardcoded as 0
-        nodes.append(children[i]) #adding all children to nodes
-        
-    #sort nodes
-    #nodes.sort(key=lambda x: x.g + x.h, reverse=True)
+    for i in range(len(children)):   
+        if isDuplicateCheck(children[i], nodes):
+            continue
+        else:
+            children[i].h = 0 #uniform cost search is A* w/ h being hardcoded as 0
+            nodes.append(children[i]) #adding all children to nodes
 
 def misplacedTileQueue(nodes, children):
     for i in range(len(children)):
-        #bisect.insort(nodes, children[i])
-        children[i].h = misplacedTileH(children[i].currentPuzzleState)
-        #nodes.append(children[i]) #adding all children to nodes
+        if isDuplicateCheck(children[i], nodes):
+            continue
+        else:
+            children[i].h = misplacedTileH(children[i].currentPuzzleState)
+            nodes.append(children[i]) #adding all children to nodes
 
 def manhattanDistanceQueue(nodes, children):
     for i in range(len(children)):
-        #bisect.insort(nodes, children[i])
-        #children[i].h = manhattanH(children[i].currentPuzzleState)
-        manhattanH(children[i].currentPuzzleState)
-        #nodes.append(children[i]) #adding all children to nodes
+        if isDuplicateCheck(children[i], nodes):
+            continue
+        else:
+            children[i].h = manhattanH(children[i].currentPuzzleState)
+            nodes.append(children[i]) #adding all children to nodes
 
 def misplacedTileH(currentPuzzleState): #return h(n) of current puzzle state
     misplacedTileH = 0
@@ -95,47 +108,46 @@ def manhattanH(currentPuzzleState):
                     continue
                 elif currentPuzzleState[x][y] != puzzleGoalState[x][y]: #check to see if tile in current puzzle state is the correct tile (compared to goal state)
                     correctTilePositon = findCoordinates(puzzleGoalState, currentPuzzleState[x][y])
-                    print("correctTilePositon: ")
-                    print(correctTilePositon)
 
                     xDistance = abs(correctTilePositon[0] - x)
                     yDistance = abs(correctTilePositon[1] - y)
 
                     manhattanH += xDistance + yDistance
-    print(str(manhattanH))
     return manhattanH
 
 def genSearchAlg(problem, typeOfHueristic): #problem operator is type of heuristic search
     keepGoing = True
     problemOperators = ['up', 'right', 'down', 'left']
+    expanded = []
 
-    firstNode = Node(problem, None, 0, 0)
-    secondNode = Node([1,2,3], problem, 0, 0)
+    firstNode = Node(problem, 0, 0, None)
     nodes = [firstNode]
 
     while keepGoing: #loop will continue until goal state is found or all tested every possible node
         if empty(nodes):
             keepGoing = False
-            return "failure" #proved that there is no solution
+            print("failure") #proved that there is no solution
         
         node = removeFront(nodes)
+        expanded.append(node) #keep track of the nodes already expanded
+
+        print("The best state to expand with g(n) = " + str(node.g) + " and h(n) = " + str(node.h) + " is...")
+        print(node.currentPuzzleState)
 
         if goalState(node.currentPuzzleState):
-            return "found solution" #return node
+            print("found solution") #return node
 
         if typeOfHueristic == 0: #uniform cost search
-            print("uniform cost search")
-            nodes = uniformCostSearchQueue(nodes, expand(node, problemOperators))
+            uniformCostSearchQueue(nodes, expand(node, problemOperators, expanded))
         elif typeOfHueristic == 1: #A* w/ Misplaced Tile heuristic
-            print("A* w/ Misplaced Tile heuristic")
-            nodes = misplacedTileQueue(nodes, expand(node, problemOperators))
+            misplacedTileQueue(nodes, expand(node, problemOperators, expanded))
         elif typeOfHueristic == 2: #A* w/ Manhattan Distance heuristic
-            print("A* w/ Manhattan Distance heuristic")
-            nodes = manhattanDistanceQueue(nodes, expand(node, problemOperators))
+            manhattanDistanceQueue(nodes, expand(node, problemOperators, expanded))
+        nodes.sort(key=lambda x: x.g + x.h)
 
 if __name__ == "__main__":
     puzzleGoalState = [[1,2,3],[4,5,6],[7,8,0]] #only one possible goal state for 8 puzzle
     
-    testUserInput = [[3,2,8],[4,5,6],[7,1,0]]
+    testUserInput = [[1,2,3],[5,0,6],[4,7,8]]
 
-    genSearchAlg(testUserInput, 2)
+    genSearchAlg(testUserInput, 0)
