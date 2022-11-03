340d2914e8bf7b121574c4256d571c4f72fdd0bd
Jeana-T <jeana2019@gmail.com>
2022-11-02 22:41:48 -0700

    completed! take in user input

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import copy
-import bisect
 
 class Node: #Creating a Node class that contains the current puzzle board state, the parent puzzle state, h score, and g score.
     def __init__(self, currentPuzzleState, h, g, previousMove):
@@ -30,7 +29,7 @@ def findCoordinates(puzzleState, value): #finds the location of specified value 
             if puzzleState[x][y] == value:
                 return (x,y) #returns coordinates of specified value
 
-def isDuplicateCheck(puzzleState, listNodes):
+def isDuplicateCheck(puzzleState, listNodes): #check to see if puzzleState already exists in passed in node list
     for g in range(len(listNodes)):
         if puzzleState == listNodes[g].currentPuzzleState:
             return True
@@ -40,11 +39,9 @@ def expand(node, problemOperators, expanded):
     blankTile = findCoordinates(node.currentPuzzleState, 0) #coordinates of 0 (blank tile) 
     x, y = blankTile[0], blankTile[1] # x -> column # of blank tile, y -> row # of blank tile
 
-    moves = [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)] #up, right, down, left (only possible moves in this puzzle)
+    moves = [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)] #up, right, down, left
     children = [] #array that will hold all children nodes of the current node
-    skipMove = -1
-
-    currentDepth = node.g
+    skipMove = -1 #impossible for move to be -1, update later on
 
     if node.g != 0:
         previousMove = problemOperators.index(node.previousMove) #find index of the previous move in operators list
@@ -68,9 +65,11 @@ def expand(node, problemOperators, expanded):
             #swap selected tile (at a, b) and blank tile
             nodeStateCopy[x][y], nodeStateCopy[a][b] = nodeStateCopy[a][b], nodeStateCopy[x][y]
 
-            if isDuplicateCheck(nodeStateCopy, expanded):
+            #print("Child Expanded: ", nodeStateCopy)
+
+            if isDuplicateCheck(nodeStateCopy, expanded): #check to see if puzzle state has already been expanded
                 continue
-            elif skipMove == i:
+            elif skipMove == i: #move on to avoid repeating parent state
                 continue
             else:
                 #add temp node (with switched tiles) to children array
@@ -79,7 +78,7 @@ def expand(node, problemOperators, expanded):
 
 def uniformCostSearchQueue(nodes, children):    
     for i in range(len(children)):   
-        if isDuplicateCheck(children[i], nodes):
+        if isDuplicateCheck(children[i], nodes): #check if child already exists in nodes
             continue
         else:
             children[i].h = 0 #uniform cost search is A* w/ h being hardcoded as 0
@@ -87,10 +86,10 @@ def uniformCostSearchQueue(nodes, children):
 
 def misplacedTileQueue(nodes, children):
     for i in range(len(children)):
-        if isDuplicateCheck(children[i], nodes):
+        if isDuplicateCheck(children[i], nodes): #check if child already exists in nodes
             continue
         else:
-            children[i].h = misplacedTileH(children[i].currentPuzzleState)
+            children[i].h = misplacedTileH(children[i].currentPuzzleState) #setting child node h using misplaced tile heuristic
             nodes.append(children[i]) #adding all children to nodes
 
 def manhattanDistanceQueue(nodes, children):
@@ -98,7 +97,7 @@ def manhattanDistanceQueue(nodes, children):
         if isDuplicateCheck(children[i], nodes):
             continue
         else:
-            children[i].h = manhattanH(children[i].currentPuzzleState)
+            children[i].h = manhattanH(children[i].currentPuzzleState) #setting child node h using manhattan heuristic
             nodes.append(children[i]) #adding all children to nodes
 
 def misplacedTileH(currentPuzzleState): #return h(n) of current puzzle state
@@ -123,19 +122,21 @@ def manhattanH(currentPuzzleState):
                 elif currentPuzzleState[x][y] != puzzleGoalState[x][y]: #check to see if tile in current puzzle state is the correct tile (compared to goal state)
                     correctTilePositon = findCoordinates(puzzleGoalState, currentPuzzleState[x][y])
 
-                    xDistance = abs(correctTilePositon[0] - x)
-                    yDistance = abs(correctTilePositon[1] - y)
+                    xDistance = abs(correctTilePositon[0] - x) #horizontal distance from correct position
+                    yDistance = abs(correctTilePositon[1] - y) #vertical distance from correct position
 
-                    manhattanH += xDistance + yDistance
+                    manhattanH += xDistance + yDistance #summing total distance of how far tile is
     return manhattanH
 
 def genSearchAlg(problem, typeOfHueristic): #problem operator is type of heuristic search
     keepGoing = True
-    problemOperators = ['up', 'right', 'down', 'left']
+    problemOperators = ['up', 'right', 'down', 'left'] #all possible moves in puzzle
     expanded = []
+    nodeExpandedCount = 0
 
     firstNode = Node(problem, 0, 0, None)
     nodes = [firstNode]
+    currentNodesMax = 1
 
     while keepGoing: #loop will continue until goal state is found or all tested every possible node
         if empty(nodes):
@@ -144,12 +145,19 @@ def genSearchAlg(problem, typeOfHueristic): #problem operator is type of heurist
             return
         node = removeFront(nodes)
         expanded.append(node) #keep track of the nodes already expanded
+        nodeExpandedCount += 1
+
+        if (len(nodes) > currentNodesMax):
+            currentNodesMax = len(nodes)
 
         print("The best state to expand with g(n) = " + str(node.g) + " and h(n) = " + str(node.h) + " is...")
         print(node.currentPuzzleState)
 
-        if goalState(node.currentPuzzleState):
-            print("found solution") #return node
+        if goalState(node.currentPuzzleState): #found solution!!!
+            print("found solution")
+            print("Solution depth was " + str(node.g))
+            print("Number of nodes expanded: " + str(nodeExpandedCount))
+            print("Max Queue Size: " + str(currentNodesMax))
             return
         if typeOfHueristic == 0: #uniform cost search
             uniformCostSearchQueue(nodes, expand(node, problemOperators, expanded))
@@ -157,11 +165,31 @@ def genSearchAlg(problem, typeOfHueristic): #problem operator is type of heurist
             misplacedTileQueue(nodes, expand(node, problemOperators, expanded))
         elif typeOfHueristic == 2: #A* w/ Manhattan Distance heuristic
             manhattanDistanceQueue(nodes, expand(node, problemOperators, expanded))
-        nodes.sort(key=lambda x: x.g + x.h)
+        nodes.sort(key=lambda x: x.g + x.h) #sorting by g+h, which is f
 
 if __name__ == "__main__":
     puzzleGoalState = [[1,2,3],[4,5,6],[7,8,0]] #only one possible goal state for 8 puzzle
     
-    testUserInput = [[7,1,2],[4,8,5],[6,3,0]]
+    algorithmSelection = input("Please select the algorithm that you would to use.\n (1) for Uniform Cost Search\n (2) for Misplaced Tile\n (3) for Manhattan Distance\n")
 
-    genSearchAlg(testUserInput, 2)
+    print("Now enter the puzzle one row at a time. Seperate numbers with a space. Use 0 to represent the blank tile")
+    firstRowString = raw_input("Enter the first row: ")
+    secondRowString = raw_input("Enter the second row: ")
+    thirdRowString = raw_input("Enter the third row: ")
+    
+    #splitting up the string by spaces
+    firstRowList = firstRowString.split(' ')
+    secondRowList = secondRowString.split(' ')
+    thirdRowList = thirdRowString.split(' ')
+
+    #changing list to list of int
+    row1numList = [eval(i) for i in firstRowList]
+    row2numList = [eval(i) for i in secondRowList]
+    row3numList = [eval(i) for i in thirdRowList]
+
+    puzzle = [row1numList, row2numList, row3numList]
+    
+    #algorithmSelection = 2
+    #puzzle = [[1,3,2],[4,5,6],[7,8,0]]
+
+    genSearchAlg(puzzle, algorithmSelection)
